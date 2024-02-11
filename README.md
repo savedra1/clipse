aa45a901112ec5fc1011ac99b2f773da2b1b65d3
savedram <michael.savedra@ovo.com>
2024-02-11 16:26:54 +0000

    Attempting to suppress nohup output

---
+++ README.md
@@ -0,0 +1,8 @@
+# TODO
+- Make sure existing nohup process always killed before `--listen` is started
+- Investigate single binary solution without exec (dont think thi sis poss)
+- Update code to create JSON file in path when non-existent 
+- Reformat data and time in item description
+- Config added to json file, adjustable with CLI args
+- Nix package
+- Makefile installation
--- selector/main.go
+++ selector/main.go
@@ -4,7 +4,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"os/exec"
+	"os/signal"
 	"strings"
+	"syscall"
+	"time"
 
 	"github.com/atotto/clipboard"
 	"github.com/charmbracelet/bubbles/key"
@@ -12,6 +16,115 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
+
+// ##################### LISTENER SECTION ####################### //
+// Data struct for storing clipboard strings
+type Data struct {
+	ClipboardHistory []ClipboardItem `json:"clipboardHistory"`
+}
+
+// ClipboardItem struct for individual clipboard history item
+type ClipboardItem struct {
+	Value    string `json:"value"`
+	Recorded string `json:"recorded"`
+}
+
+func runListener() error {
+	// Listen for SIGINT (Ctrl+C) and SIGTERM signals to properly close the program
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	// Load existing data from file, if any
+	var data Data
+	err := loadDataFromFile("../history.json", &data)
+	if err != nil {
+		fmt.Println("Error loading data from file:", err)
+	}
+
+	// Start a goroutine to continuously monitor clipboard changes
+	go func() {
+		for {
+			// Get the current clipboard content
+			text, err := clipboard.ReadAll()
+			if err != nil {
+				fmt.Println("Error reading clipboard:", err)
+			}
+
+			// If clipboard content is not empty and not already in the list, add it
+			if text != "" && !contains(data.ClipboardHistory, text) {
+				// If the length exceeds 50, remove the oldest item
+				if len(data.ClipboardHistory) >= 50 {
+					data.ClipboardHistory = data.ClipboardHistory[1:] // Remove the oldest item (first element)
+				}
+				timeNow := time.Now().UTC().String()
+				item := ClipboardItem{Value: text, Recorded: timeNow}
+				data.ClipboardHistory = append([]ClipboardItem{item}, data.ClipboardHistory...)
+				fmt.Println("Added to clipboard history:", text)
+
+				// Save data to file
+				err := saveDataToFile("../history.json", data)
+				if err != nil {
+					fmt.Println("Error saving data to file:", err)
+				}
+			}
+
+			// Check for updates every 0.1 second
+			duration := 100 * time.Millisecond / 10
+			time.Sleep(duration)
+		}
+	}()
+
+	fmt.Println("Clipboard history listener running... Press Ctrl+C to exit.")
+
+	// Wait for SIGINT or SIGTERM signal
+	<-interrupt
+	fmt.Println("Exiting...")
+	return nil
+}
+
+// contains checks if a string exists in a slice of strings
+func contains(slice []ClipboardItem, str string) bool {
+	for _, item := range slice {
+		if item.Value == str {
+			return true
+		}
+	}
+	return false
+}
+
+// loadDataFromFile loads data from a JSON file
+func loadDataFromFile(filename string, data *Data) error {
+	file, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(data)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// saveDataToFile saves data to a JSON file
+func saveDataToFile(filename string, data Data) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	err = encoder.Encode(data)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// ##################### LISTENER SECTION ####################### //
 
 var (
 	appStyle = lipgloss.NewStyle().Padding(1, 2)
@@ -346,6 +459,7 @@ func main() {
 	help := "--help"
 	listen := "--listen"
 	clear := "--clear"
+	listenStart := "--start"
 
 	if len(os.Args) > 1 {
 		switch os.Args[1] {
@@ -353,12 +467,26 @@ func main() {
 			fmt.Println("Place holder")
 			os.Exit(0)
 		case listen:
-			fmt.Println("Place holder")
+			cmd := exec.Command("pkill", os.Args[0])
+			cmd.Run() // Kill existing clipboard processes
+			cmd = exec.Command("nohup", "go", "run", os.Args[0], "--start", "&")
+			//cmd = exec.Command("nohup", "sh", "-c", "go run "+os.Args[0]+" --start >/dev/null 2>&1 &")
+
+			if err := cmd.Start(); err != nil {
+				fmt.Println("Error starting clipboard listener:", err)
+				os.Exit(1)
+			}
+			fmt.Println("Starting clipboard listener...")
 			os.Exit(0)
 		case clear:
 			fmt.Println("Place holder")
 			os.Exit(0)
-
+		case listenStart:
+			err := runListener()
+			if err != nil {
+				fmt.Println(err)
+			}
+			os.Exit(0)
 		default:
 			fmt.Println("Arg not recognised. Try `clipboard --help` for more details.")
 			os.Exit(0)
