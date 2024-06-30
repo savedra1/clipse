d420f483ccba072917f5bf9b3b02e5111127bc74
Michael Savedra <99875823+savedra1@users.noreply.github.com>
2024-06-30 21:57:35 +0100

    fix: refactor KillProcess func (#87)

--- shell/constants.go
+++ shell/constants.go
@@ -1,5 +1,12 @@
 package shell
 
 const (
-	listenCmd = "--listen-shell"
+	listenCmd     = "--listen-shell"
+	pgrepCmd      = "pgrep -a clipse"
+	wlVersionCmd  = "wl-copy"
+	wlCopyImgCmd  = "wl-copy -t image/png <"
+	wlPasteImgCmd = "wl-paste -t image/png >"
+	xVersionCmd   = "xclip -v"
+	xCopyImgCmd   = "xclip -selection clipboard -t image/png -i"
+	xPasteImgCmd  = "xclip -selection clipboard -t image/png -o >"
 )
