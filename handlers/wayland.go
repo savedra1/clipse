751ac15ec614d93ec2334d9ec453da3911cb1224
Michael Savedra <99875823+savedra1@users.noreply.github.com>
2024-07-13 19:32:25 +0100

    fix: images losing pin status (#103)

--- handlers/wayland.go
+++ handlers/wayland.go
@@ -4,13 +4,13 @@ import (
 	"bytes"
 	"fmt"
 	"image"
-	"image/gif"
 	"image/jpeg"
 	"image/png"
 	"io"
 	"os"
 	"path/filepath"
 	"strconv"
+	"strings"
 
 	"github.com/savedra1/clipse/config"
 	"github.com/savedra1/clipse/utils"
@@ -23,7 +23,7 @@ func StoreWLData() {
 		return
 	}
 
-	dataType = "text"
+	dataType = "text" // defined in defaultHandler
 	img, format, err := image.Decode(bytes.NewReader(input))
 	if err == nil {
 		dataType = "image"
@@ -38,14 +38,20 @@ func StoreWLData() {
 		if err := config.AddClipboardItem(inputStr, "null"); err != nil {
 			utils.LogERROR(fmt.Sprintf("failed to add new item `( %s )` | %s", input, err))
 		}
-	case "image":
-		fileName := fmt.Sprintf("%s-%s.%s", strconv.Itoa(len(input)), utils.GetTimeStamp(), format)
-		itemTitle := fmt.Sprintf("%s %s", imgIcon, fileName)
-		filePath := filepath.Join(config.ClipseConfig.TempDirPath, fileName)
 
-		if err := config.AddClipboardItem(itemTitle, filePath); err != nil {
-			utils.LogERROR(fmt.Sprintf("failed to save image | %s", err))
-		}
+	case "image":
+		/*
+			When saving image data from the stdin using wl-paste --watch,
+			the byte size if different to when the image data is copied
+			from the saved file with wl-copy -t image/path.
+			This means to maintain consistency with identifying duplicates
+			we need to save a temporary image file, then update the file name
+			using the file size as the identifier, so any duplicates can be
+			auto-removed during the AddClipboardItem call in the same way
+			as non-wayland specific data.
+		*/
+		fileName := fmt.Sprintf("%s.%s", utils.GetTimeStamp(), format)
+		filePath := filepath.Join(config.ClipseConfig.TempDirPath, fileName)
 
 		out, err := os.Create(filePath)
 		if err != nil {
@@ -60,10 +66,8 @@ func StoreWLData() {
 			err = png.Encode(out, img)
 		case "jpeg", "jpg":
 			err = jpeg.Encode(out, img, nil)
-		case "gif":
-			err = gif.Encode(out, img, nil)
 		default:
-			// If format is not recognized, default to PNG
+			// default to PNG if format not recognized
 			err = png.Encode(out, img)
 		}
 
@@ -71,5 +75,32 @@ func StoreWLData() {
 			utils.LogERROR(fmt.Sprintf("failed to encode img data: %s", err))
 			return
 		}
+
+		fileInfo, err := os.Stat(filePath)
+		if err != nil {
+			utils.LogERROR(fmt.Sprintf("failed to read new image file: %s", err))
+			return
+		}
+
+		fileSize := fileInfo.Size()
+		updatedFileName := fmt.Sprintf(
+			"%s-%s.%s",
+			strconv.Itoa(int(fileSize)),
+			strings.Split(fileName, ".")[0],
+			format,
+		)
+		updatedFilePath := filepath.Join(config.ClipseConfig.TempDirPath, updatedFileName)
+
+		if err = os.Rename(filePath, updatedFilePath); err != nil {
+			utils.LogERROR(fmt.Sprintf("failed to rename new image file: %s", err))
+			return
+		}
+
+		itemTitle := fmt.Sprintf("%s %s", imgIcon, updatedFileName)
+
+		if err := config.AddClipboardItem(itemTitle, updatedFilePath); err != nil {
+			utils.LogERROR(fmt.Sprintf("failed to save image | %s", err))
+		}
+
 	}
 }
