31d08b7b22733a313a15be573cd0cf8ef2fab814
savedram <michael.savedra@ovo.com>
2024-10-04 16:35:01 -0400

    release: 1.0.9 -> 1.1.0

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ import (
 )
 
 var (
-	version     = "v1.0.9"
+	version     = "v1.1.0"
 	help        = flag.Bool("help", false, "Show help message.")
 	v           = flag.Bool("v", false, "Show app version.")
 	add         = flag.Bool("a", false, "Add the following arg to the clipboard history.")
