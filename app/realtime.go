a3c996a6e7abb43f170baec015c9f6ebdc97f0d0
supritsj@Arch <supritsj05@gmail.com>
2024-11-29 15:17:47 +0530

    basic error handling

--- app/realtime.go
+++ app/realtime.go
@@ -7,19 +7,27 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 
 	"github.com/savedra1/clipse/config"
+	"github.com/savedra1/clipse/utils"
 )
 
 type ReRender struct{}
 
 func (m Model) ListenRealTime(p *tea.Program) {
 	historyPath := config.ClipseConfig.HistoryFilePath
-	info, _ := os.Stat(historyPath)
+	info, err := os.Stat(historyPath)
+	if err != nil {
+		utils.LogERROR("Could not get Modification time of history file, starting real time mode failed")
+		return
+	}
 	m.lastUpdated = info.ModTime()
 
 	rr := ReRender{}
 	var currModTime time.Time
 	for {
-		historyFileInfo, _ := os.Stat(historyPath)
+		historyFileInfo, err := os.Stat(historyPath)
+		if err != nil {
+			continue
+		}
 		currModTime = historyFileInfo.ModTime()
 
 		if currModTime.After(m.lastUpdated) {
