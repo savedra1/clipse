7b1483c85441d5cca9de41851373b34ef0566225
Michael Savedra <michaelsav@mo.co.uk>
2025-12-26 17:05:52 +0000

    release: v1.2.0

--- cmd/root.go
+++ cmd/root.go
@@ -17,7 +17,7 @@ import (
 )
 
 var (
-	version       = "v1.1.5"
+	version       = "v1.2.0"
 	help          = flag.Bool("help", false, "Show help message.")
 	v             = flag.Bool("v", false, "Show app version.")
 	add           = flag.Bool("a", false, "Add the following arg to the clipboard history.")
